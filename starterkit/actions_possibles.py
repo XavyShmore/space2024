6292ec0a134955bec19ffb597c984db0cf7d8201
Xavier Hamel <xavier.hamel.5@ulaval.ca>
2024-01-13 09:13:21 -0500

    Priority queue

---
+++ starterkit/actions_possibles.py
@@ -0,0 +1,61 @@
+class rotate_ship:
+    priority = 0
+    bot = None
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def do_action(self):
+        pass
+    def update_priority(self):
+        priority = 0
+
+class recharge_shields:
+    priority = 0
+    bot = None
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def do_action(self):
+        pass
+    def update_priority(self):
+        priority = 0
+
+class recharge_turrets:
+    priority = 0
+    bot = None
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def do_action(self):
+        pass
+    def update_priority(self):
+        priority = 0
+
+class shoot:
+    priority = 0
+    bot = None
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def do_action(self):
+        pass
+    def update_priority(self):
+        priority = 0
+
+class use_radar:
+    priority = 0
+    bot = None
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    def do_action(self):
+        pass
+    def update_priority(self):
+        priority = 0
+
+
